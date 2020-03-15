d5986b590b2c77d0b77a41c7d35027e9db4ff336
Joao Freitas <1160907@isep.ipp.pt>
2020-03-15 15:03:51 +0000

    Added implementation classes for table and dropdown complex types

---
+++ complex-types/src/main/java/insis/acervo/DropdownFormType.java
@@ -0,0 +1,29 @@
+package insis.acervo;
+
+import org.activiti.engine.form.AbstractFormType;
+
+public class DropdownFormType extends AbstractFormType {
+   	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2L;
+	public static final String TYPE_NAME = "dropdown";
+   
+   	public String getName() {
+   		return TYPE_NAME;
+   	}
+   
+   	@Override
+   	public Object convertFormValueToModelValue(String propertyValue) {
+   		Integer dropdown = Integer.valueOf(propertyValue);
+   		return dropdown;
+   	}
+   
+   	@Override
+   	public String convertModelValueToFormValue(Object modelValue) {
+   		if	(modelValue	== null) {
+   			return null;
+   		}
+   		return modelValue.toString();
+   	}
+   }
