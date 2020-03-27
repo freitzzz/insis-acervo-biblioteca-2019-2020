4397973434c5fa5eabb4d55148aeac56f2cfdf54
Joao Freitas <1160907@isep.ipp.pt>
2020-03-27 22:56:37 +0000

    Added class that illustrates an example of a task listener implementation

---
+++ task-listeners/src/main/java/insis/acervo/SampleTaskListener.java
@@ -0,0 +1,25 @@
+package insis.acervo;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SampleTaskListener implements TaskListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3L;
+	
+	private static Log LOGGER = LogFactory.getLog(SampleTaskListener.class);
+	
+	public void notify(DelegateTask arg0) {
+		LOGGER.info("Sample Task Listener starting");
+		
+		// Operations declared here
+		
+		LOGGER.info("Sample Task Listener ending");
+	}
+
+}
