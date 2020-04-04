3b9ac5b44c114d4bbe456a4e471b8104ea004c9a
Joao Freitas <1160907@isep.ipp.pt>
2020-04-04 20:59:31 +0100

    Added complex type for multi selection dropdown

--- complex-types/src/main/java/insis/acervo/MultiSelectionDropdownFormType.java
+++ complex-types/src/main/java/insis/acervo/MultiSelectionDropdownFormType.java
@@ -2,12 +2,12 @@ package insis.acervo;
 
 import org.activiti.engine.form.AbstractFormType;
 
-public class DropdownFormType extends AbstractFormType {
+public class MultiSelectionDropdownFormType extends AbstractFormType {
    	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 2L;
-	public static final String TYPE_NAME = "dropdown";
+	private static final long serialVersionUID = 5L;
+	public static final String TYPE_NAME = "dropdown_multi";
    
    	public String getName() {
    		return TYPE_NAME;
