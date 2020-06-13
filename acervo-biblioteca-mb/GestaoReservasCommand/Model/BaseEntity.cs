4d92d8bbd53fd4234d2ffc453c52352da9a62bcb
Rute Santos <1160663@isep.ipp.pt>
2020-06-13 14:45:56 +0100

    GestaoReservasCommand Model

---
+++ acervo-biblioteca-mb/GestaoReservasCommand/Model/BaseEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestaoReservasCommand.Model
+{
+    public abstract class BaseEntity
+    {
+        // public long Id { get; set; }
+
+        // public Boolean State { get; set; } = true;
+    }
+}
