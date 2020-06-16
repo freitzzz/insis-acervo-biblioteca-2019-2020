f2e489b2279ea47cf00d0a229cab7a2dc80900ed
Rute Santos <1160663@isep.ipp.pt>
2020-06-16 21:35:35 +0100

    GestaoReservasQuery Migrations

--- acervo-biblioteca-mb/GestaoReservasQuery/Migrations/20200616183450_InitialCreate.Designer.cs
+++ acervo-biblioteca-mb/GestaoReservasQuery/Migrations/20200616183450_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GestaoReservasQuery.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GestaoReservasQuery.Migrations
 {
     [DbContext(typeof(GestaoReservasQueryContext))]
-    partial class GestaoReservasQueryContextModelSnapshot : ModelSnapshot
+    [Migration("20200616183450_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ acervo-biblioteca-mb/GestaoReservasQuery/Migrations/20200616183450_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoReservasQuery.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Obra",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    titulo = table.Column<string>(nullable: true),
+                    estado = table.Column<int>(nullable: false),
+                    polo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obra", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    utente = table.Column<string>(nullable: true),
+                    dataInicio = table.Column<DateTime>(nullable: false),
+                    dataFim = table.Column<DateTime>(nullable: false),
+                    obraId = table.Column<long>(nullable: true),
+                    estado = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Obra_obraId",
+                        column: x => x.obraId,
+                        principalTable: "Obra",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_obraId",
+                table: "Reservas",
+                column: "obraId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Obra");
+        }
+    }
+}
