400497ba9e52025e8ad1f723fd4fc662d50de4b7
Joao Freitas <1160907@isep.ipp.pt>
2020-06-16 17:04:19 +0100

    Updated index.js template to connect to RabbitMQ server

--- acervo-biblioteca-mb/GestaoUtentesQuery/index.js
+++ acervo-biblioteca-mb/GestaoUtentesQuery/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const cors = require('cors');
 
-const app = express();
-
 const moongoose = require('mongoose');
+
+const amqp = require('amqplib/callback_api');
+
+const app = express();
 
 app.use(cors());
 
 app.listen(process.env.PORT);
+
+amqp.connect(process.env.RABBIT_MQ_CONNECTION_URL, function (error0, connection) { });
 
 moongoose.connect(process.env.GUQ_MONGODB_CONNECTION_URL, { useNewUrlParser: true });
