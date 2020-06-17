5880e799e7e998b50b7ffaf4158932bcc3156c5a
Joao Freitas <1160907@isep.ipp.pt>
2020-06-17 19:12:40 +0100

    Added unit tests for updateEstatuto function

--- acervo-biblioteca-mb/GestaoUtentesQuery/model/utente.test.js
+++ acervo-biblioteca-mb/GestaoUtentesQuery/model/utente.test.js
@@ -1,5 +1,6 @@
 const utente = require('./utente.js');
 
+// "estado" function
 
 test('if utente estatuto value is equal to 2, then estado returns "ativo" string', function () {
 
@@ -100,5 +101,169 @@ test('if utente estatuto value is less than 1, then estado returns "inativo" str
   // Assert
 
   expect(estado).toBe('inativo');
+
+});
+
+// "updateEstatuto" function
+
+test('if the new utente estatuto is greater than 6, then utente estatuto is updated to 6 and dataHoraFinalSuspensao is set as undefined', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  // Act
+
+  utenteInstance.dataHoraFinalSuspensao = currentDate;
+
+  utente.updateEstatuto(utenteInstance, 6.1);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(6);
+
+  expect(dataHoraFinalSuspensao).toBe(undefined);
+
+});
+
+test('if the new utente estatuto is 6, then utente estatuto is updated to 6 and dataHoraFinalSuspensao is set as undefined', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  // Act
+
+  utenteInstance.dataHoraFinalSuspensao = currentDate;
+
+  utente.updateEstatuto(utenteInstance, 6);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(6);
+
+  expect(dataHoraFinalSuspensao).toBe(undefined);
+
+});
+
+test('if the new utente estatuto is less than 6 and greater than 2, then utente estatuto is updated to the given value and dataHoraFinalSuspensao is set as undefined', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  const newEstatuto = 2.1
+
+  // Act
+
+  utenteInstance.dataHoraFinalSuspensao = currentDate;
+
+  utente.updateEstatuto(utenteInstance, newEstatuto);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(newEstatuto);
+
+  expect(dataHoraFinalSuspensao).toBe(undefined);
+
+});
+
+test('if the new utente estatuto is equal to 2, then utente estatuto is updated to 2 and dataHoraFinalSuspensao is set as undefined', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  // Act
+
+  utenteInstance.dataHoraFinalSuspensao = currentDate;
+
+  utente.updateEstatuto(utenteInstance, 2);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(2);
+
+  expect(dataHoraFinalSuspensao).toBe(undefined);
+
+});
+
+test('if the new utente estatuto is less than 2, then utente estatuto is updated to the given value and dataHoraFinalSuspensao is set as current date + two months', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  const currentDateInTwoMonths = new Date(currentDate.setMonth(currentDate.getMonth() + 2));
+
+  const newEstatuto = 1.9;
+
+  // Act
+
+  utente.updateEstatuto(utenteInstance, newEstatuto);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(newEstatuto);
+
+  expect(dataHoraFinalSuspensao.getFullYear()).toBe(currentDateInTwoMonths.getFullYear());
+
+  expect(dataHoraFinalSuspensao.getMonth()).toBe(currentDateInTwoMonths.getMonth());
+
+});
+
+test('if the new utente estatuto is less than 1, then utente estatuto is updated to 0.99 and dataHoraFinalSuspensao is set as undefined', function () {
+
+  // Arrange
+
+  const utenteInstance = utente.create('Albale', 'Real');
+
+  const currentDate = new Date();
+
+  // Act
+
+  utenteInstance.dataHoraFinalSuspensao = currentDate;
+
+  utente.updateEstatuto(utenteInstance, 0.98);
+
+  const estatuto = utenteInstance.estatuto;
+
+  const dataHoraFinalSuspensao = utenteInstance.dataHoraFinalSuspensao;
+
+  // Assert
+
+  expect(estatuto).toBe(0.99);
+
+  expect(dataHoraFinalSuspensao).toBe(undefined);
 
 });
