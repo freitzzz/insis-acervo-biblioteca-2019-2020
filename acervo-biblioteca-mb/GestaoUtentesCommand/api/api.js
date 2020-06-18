a6190819086241d3b445ac07b4b9ff3e6d06b6e2
Joao Freitas <1160907@isep.ipp.pt>
2020-06-18 15:12:27 +0100

    Remove dead code and expose onReporEstadoRecebido function

--- acervo-biblioteca-mb/GestaoUtentesCommand/api/api.js
+++ acervo-biblioteca-mb/GestaoUtentesCommand/api/api.js
@@ -1,46 +1,9 @@
+const eventstore = require('eventstore')();
 
-const utente = require('../model/utente.js');
+function onReporEstadoRecebido(idUtente, idBibliotecarioMor, valorEstatuto) {
 
-const mongoose = require('mongoose');
 
-const UtentesCollection = mongoose.model('utente', new mongoose.Schema(utente.schema));
 
-function updateUtente(request, response) {
-
-  const id = request.params.id;
-
-  UtentesCollection.findById(id).exec(function (error, document) {
-    if (error) {
-      response.status(404).send();
-    } else {
-
-      const body = request.body;
-
-      const utenteInstance = document.toObject();
-
-      const newEstatuto = body.estatuto;
-
-      if (typeof newEstatuto === 'number') {
-
-        utente.updateEstatuto(utenteInstance, newEstatuto);
-
-      }
-
-      UtentesCollection.updateOne({ _id: id }, utenteInstance).exec(function (error, _) {
-        if (error) {
-          response.status(500).send();
-        } else {
-
-          utenteInstance.toObject = () => utenteInstance;
-
-          const utenteView = utenteDocumentToView(utenteInstance);
-
-          response.status(200).send(utenteView);
-
-        }
-      });
-    }
-  });
 }
 
-exports.updateUtente = updateUtente;
+exports.onReporEstadoRecebido = onReporEstadoRecebido;
