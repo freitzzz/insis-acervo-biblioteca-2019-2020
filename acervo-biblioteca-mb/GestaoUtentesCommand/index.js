421870d626d78b10f3c42a28888d37b4e8e4eaf3
Joao Freitas <1160907@isep.ipp.pt>
2020-06-18 23:15:52 +0100

    Added rabbitmq connection setup on GestaoUtentesCommand

--- acervo-biblioteca-mb/GestaoUtentesCommand/index.js
+++ acervo-biblioteca-mb/GestaoUtentesCommand/index.js
@@ -10,81 +10,151 @@ const app = express();
 
 const eventstore = require('eventstore')();
 
-const publishCallback = function (message, data) {
+const reporEstadoExchange = process.env.REPOR_ESTADO_EXCHANGE || 'repor_estado';
 
-}
+amqp.connect(process.env.RABBIT_MQ_CONNECTION_URL, function (errorConnectRabbitMQ, connection) {
 
-app.use(cors());
+  if (errorConnectRabbitMQ) {
 
-app.use(express.json());
+    console.error(`Failed to connect to RabbitMQ server with connection string: ${process.env.RABBIT_MQ_CONNECTION_URL} due to: ${errorConnectRabbitMQ}`);
 
-app.get('/commands/:id', function (request, response, _) {
+    throw errorConnectRabbitMQ;
 
-  const streamId = request.params.id;
+  } else {
 
-  eventstore.getEventStream(streamId, function (error, stream) {
+    connection.createChannel(function (errorCreateChannel, channel) {
 
-    if (error) {
+      if (errorCreateChannel) {
 
-      console.log(`Got error retrieving events of stream: ${streamId} due to: ${error}`);
+        console.error(`Failed to create channel on RabbitMQ server due to: ${errorCreateChannel}`);
 
-      response.status(404).send();
+        throw errorCreateChannel;
 
-    } else {
-
-      console.log(`Got events history of stream: ${streamId}`);
-
-      const events = stream.events[0].payload;
-
-      if (events.repor_estado_utente_nao_encontrado) {
-
-        response.status(404).send({ message: `Utente with id: ${events.repor_estado_utente_nao_encontrado} not found` });
-
-      } else if (events.repor_estado_bibliotecario_mor_nao_encontrado) {
-
-        response.status(404).send({ message: `Bibliotecário-Mor with id: ${events.repor_estado_bibliotecario_mor_nao_encontrado} not found` });
-      } else if (events.repor_estado_nao_autorizado) {
-
-        response.status(401).send({ message: `Bibliotecário-Mor with id: ${events.repor_estado_nao_autorizado} is not allowed to perform repor utente estado operation` });
-      } else if (events.repor_estado_estatuto_value_not_enough) {
-
-        response.status(400).send({ message: `Estatuto with value: ${events.repor_estado_estatuto_value_not_enough} is not enough to perform repor utente estado operation` });
-      } else if (events.repor_estado_utente_not_inativo) {
-
-        response.status(400).send({ message: `Utente with id: ${events.repor_estado_utente_not_inativo} is not inativo` });
-
-      } else if (events.repor_estado_nao_realizado) {
-
-        response.status(500).send({ message: `Operation failed due to server failure. Reason: ${events.repor_estado_nao_realizado}` });
-      } else if (events.repor_estado_realizado) {
-
-        response.status(200).send({ url: `/utentes/${events.repor_estado_realizado}` });
       } else {
 
-        response.status(202).send({ url: `/commands/${streamId}` });
+        const publishCallback = function (message, data) {
+
+          let exchange;
+
+          if (message.startsWith('repor_estado')) {
+
+            exchange = reporEstadoExchange;
+
+          }
+
+          channel.publish(exchange, message, Buffer.from(data));
+        }
+
+        channel.assertExchange(reporEstadoExchange, 'direct', {
+          durable: true
+        });
+
+        channel.assertQueue('', {
+          exclusive: false,
+          durable: true
+        }, function (errorAssertingQueue, queue) {
+
+          if (errorAssertingQueue) {
+
+            console.error(`Failed asserting queue due to ${errorAssertingQueue}`);
+
+            throw errorAssertingQueue;
+
+          } else {
+
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_utente_nao_encontrado');
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_bibliotecario_mor_nao_encontrado');
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_nao_autorizado');
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_autorizado');
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_realizado');
+            channel.bindQueue(queue.queue, reporEstadoExchange, 'repor_estado_nao_realizado');
+
+            channel.consume(queue.queue, function (message) {
+              switch (message.fields.routingKey) {
+                default:
+                  console.log(message);
+                  break;
+              }
+            }, {
+              noAck: false
+            });
+          }
+        });
+
+        app.use(cors());
+
+        app.use(express.json());
+
+        app.get('/commands/:id', function (request, response, _) {
+
+          const streamId = request.params.id;
+
+          eventstore.getEventStream(streamId, function (error, stream) {
+
+            if (error) {
+
+              console.log(`Got error retrieving events of stream: ${streamId} due to: ${error}`);
+
+              response.status(404).send();
+
+            } else {
+
+              console.log(`Got events history of stream: ${streamId}`);
+
+              const events = stream.events[0].payload;
+
+              if (events.repor_estado_utente_nao_encontrado) {
+
+                response.status(404).send({ message: `Utente with id: ${events.repor_estado_utente_nao_encontrado} not found` });
+
+              } else if (events.repor_estado_bibliotecario_mor_nao_encontrado) {
+
+                response.status(404).send({ message: `Bibliotecário-Mor with id: ${events.repor_estado_bibliotecario_mor_nao_encontrado} not found` });
+              } else if (events.repor_estado_nao_autorizado) {
+
+                response.status(401).send({ message: `Bibliotecário-Mor with id: ${events.repor_estado_nao_autorizado} is not allowed to perform repor utente estado operation` });
+              } else if (events.repor_estado_estatuto_value_not_enough) {
+
+                response.status(400).send({ message: `Estatuto with value: ${events.repor_estado_estatuto_value_not_enough} is not enough to perform repor utente estado operation` });
+              } else if (events.repor_estado_utente_not_inativo) {
+
+                response.status(400).send({ message: `Utente with id: ${events.repor_estado_utente_not_inativo} is not inativo` });
+
+              } else if (events.repor_estado_nao_realizado) {
+
+                response.status(500).send({ message: `Operation failed due to server failure. Reason: ${events.repor_estado_nao_realizado}` });
+              } else if (events.repor_estado_realizado) {
+
+                response.status(200).send({ url: `/utentes/${events.repor_estado_realizado}` });
+              } else {
+
+                response.status(202).send({ url: `/commands/${streamId}` });
+
+              }
+
+            }
+
+          });
+
+        });
+
+        app.put('/utentes/:id', function (request, response, _) {
+
+          const body = request.body;
+
+          const idUtente = request.params.id;
+
+          const idBibliotecarioMor = request.headers.id_bibliotecario_mor;
+
+          const valorEstatuto = body.estatuto;
+
+          api.onReporEstadoRecebido(eventstore, idUtente, idBibliotecarioMor, valorEstatuto, publishCallback, response);
+
+        });
+
+        app.listen(process.env.PORT);
 
       }
-
-    }
-
-  });
-
+    });
+  }
 });
-
-app.put('/utentes/:id', function (request, response, _) {
-
-  const body = request.body;
-
-  const idUtente = request.params.id;
-
-  const idBibliotecarioMor = request.headers.id_bibliotecario_mor;
-
-  const valorEstatuto = body.estatuto;
-
-  api.onReporEstadoRecebido(eventstore, idUtente, idBibliotecarioMor, valorEstatuto, publishCallback, response);
-
-});
-
-app.listen(process.env.PORT);
-
-amqp.connect(process.env.RABBIT_MQ_CONNECTION_URL, function (error0, connection) { });
