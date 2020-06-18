90a7e277890df14f8bc17d29e8c77e9e5c966bd6
Joao Freitas <1160907@isep.ipp.pt>
2020-06-18 15:28:00 +0100

    Experiment using getNewId function

--- acervo-biblioteca-mb/GestaoUtentesCommand/api/api.js
+++ acervo-biblioteca-mb/GestaoUtentesCommand/api/api.js
@@ -2,7 +2,17 @@ const eventstore = require('eventstore')();
 
 function onReporEstadoRecebido(idUtente, idBibliotecarioMor, valorEstatuto) {
 
+  console.log(`onReporEstadoRecebido called with $idUtente: ${idUtente}, $idBibliotecarioMor: ${idBibliotecarioMor}, $valorEstatuto: ${valorEstatuto}`);
 
+  eventstore.getNewId(function (idError, newId) {
+
+    if (idError) {
+      console.error(`Failed to get new stream id due to: ${idError}`);
+    } else {
+      console.log(`Got new stream id: ${newId}`);
+    }
+
+  });
 
 }
 
