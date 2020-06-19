9ff29a2782b0e702fddcf69dad3c3198c0e82336
Joao Freitas <1160907@isep.ipp.pt>
2020-06-19 22:46:03 +0100

    Removed unused functions from utente.js and added hasPermissionsToReporEstadoUtente function

--- acervo-biblioteca-mb/GestaoAutorizacoesCommand/model/utente.js
+++ acervo-biblioteca-mb/GestaoAutorizacoesCommand/model/utente.js
@@ -1,11 +1,10 @@
 
-const estados = ['inativo', 'suspenso', 'ativo'];
-
 const schema = {
   primeiroNome: String,
   ultimoNome: String,
   estatuto: Number,
-  dataHoraFinalSuspensao: Date
+  dataHoraFinalSuspensao: Date,
+  permissoesEspeciais: Boolean
 };
 
 function create(primeiroNome, ultimoNome) {
@@ -24,39 +23,11 @@ function create(primeiroNome, ultimoNome) {
 
 }
 
+function hasPermissionsToReporEstadoUtente(utente) {
 
-function isUtenteInativo(utente) {
-
-  const utenteEstado = estado(utente);
-
-  return utenteEstado === estados[0];
+  return utente.permissoesEspeciais;
 
 }
 
-function isEstatutoValueEnoughToReporEstado(estatuto) {
-
-  return estatuto >= 2;
-
-}
-
-function estado(utente) {
-
-  const estatuto = utente.estatuto;
-
-  const dataHoraFinalSuspensao = utente.dataHoraFinalSuspensao;
-
-  if (estatuto < 1) {
-    return estados[0];
-  } else if (estatuto < 2 && dataHoraFinalSuspensao > new Date()) {
-    return estados[1];
-  } else {
-    return estados[2];
-  }
-
-}
-
-
-module.exports.isEstatutoValueEnoughToReporEstado = isEstatutoValueEnoughToReporEstado;
-module.exports.isUtenteInativo = isUtenteInativo;
 module.exports.create = create;
-module.exports.estado = estado;
+module.exports.hasPermissionsToReporEstadoUtente = hasPermissionsToReporEstadoUtente;
