d165ec76d15ab8d958a2e9ab1fd400433ac1b4ab
Rute Santos <1160663@isep.ipp.pt>
2020-06-21 23:04:42 +0100

    GestaoReservasCommand Events

---
+++ acervo-biblioteca-mb/GestaoReservasCommand/Events/EventName.cs
@@ -0,0 +1,17 @@
+namespace GestaoReservasCommand.Events
+{
+    public class EventName
+    {
+        private EventName(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static EventName EmprestimoSobreposto { get { return new EventName("emprestimo_sobreposto"); } }
+        public static EventName EmprestimoNaoSobreposto { get { return new EventName("emprestimo_nao_sobreposto"); } }
+        public static EventName UtenteAutorizado { get { return new EventName("utente_autorizado"); } }
+        public static EventName UtenteNaoAutorizado { get { return new EventName("utente_nao_autorizado"); } }
+        public static EventName ReservaRecebida { get { return new EventName("reserva_recebida"); } }
+        public static EventName ReservaNaoRealizada { get { return new EventName("reserva_nao_realizada"); } }
+        public static EventName ReservaRealizada { get { return new EventName("reserva_realizada"); } }
+    }
+}
--- acervo-biblioteca-mb/GestaoReservasCommand/Events/UtenteAutorizadoEvent.cs
+++ acervo-biblioteca-mb/GestaoReservasCommand/Events/UtenteAutorizadoEvent.cs
@@ -4,22 +4,21 @@ using GestaoReservasCommand.DTO;
 
 namespace GestaoReservasCommand.Event
 {
-    public class EmprestimoNaoSobrepostoEvent : BaseEvent
+    public class UtenteAutorizadoEvent : BaseEvent
     {
         public string utente { get; set; }
         public DateTime dataInicio { get; set; }
         public DateTime dataFim { get; set; }
         public string obra { get; set; }
-        public List<ObraDTO> obrasSemEmprestimo { get; set; }
-
-        public EmprestimoNaoSobrepostoEvent() { }
-        public EmprestimoNaoSobrepostoEvent(string utente, DateTime dataInicio, DateTime dataFim, string obra, List<ObraDTO> obrasSemEmprestimo)
-        {
+        public List<ObraDTO> obrasAutorizadas { get; set; }
+        
+        public UtenteAutorizadoEvent(){}
+        public UtenteAutorizadoEvent(string utente, DateTime dataInicio, DateTime dataFim, string obra, List<ObraDTO> obrasAutorizadas){
             this.utente = utente;
             this.dataInicio = dataInicio;
             this.dataFim = dataFim;
             this.obra = obra;
-            this.obrasSemEmprestimo = obrasSemEmprestimo;
+            this.obrasAutorizadas = obrasAutorizadas;
         }
     }
 }
