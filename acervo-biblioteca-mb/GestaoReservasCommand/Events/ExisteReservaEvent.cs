1263025a38c7528f171e2d5ab96eca54fdd26078
Rute Santos <1160663@isep.ipp.pt>
2020-06-22 11:15:07 +0100

    GestaoReservasCommand Events

--- acervo-biblioteca-mb/GestaoReservasCommand/Events/ExisteReservaEvent.cs
+++ acervo-biblioteca-mb/GestaoReservasCommand/Events/ExisteReservaEvent.cs
@@ -4,14 +4,16 @@ using GestaoReservasCommand.DTO;
 
 namespace GestaoReservasCommand.Event
 {
-    public class ExisteReservaEvent
+    public class ExisteReservaEvent : BaseEvent
     {
         public string utente { get; set; }
         public DateTime dataInicio { get; set; }
         public DateTime dataFim { get; set; }
         public string obra { get; set; }
         public List<ReservaDTO> listaReservas { get; set; }
-        public ExisteReservaEvent(String utente, DateTime dataInicio, DateTime dataFim, String obra, List<ReservaDTO> listaReservas){
+        public ExisteReservaEvent(string utente, DateTime dataInicio, DateTime dataFim, string obra, List<ReservaDTO> listaReservas, string streamId)
+        {
+            this.streamId = streamId;
             this.utente = utente;
             this.dataInicio = dataInicio;
             this.dataFim = dataFim;
