2f854a1eeccb44d5e422a7e33a172d01fc011d92
Rute Santos <1160663@isep.ipp.pt>
2020-06-22 11:25:35 +0100

    GestaoReservasCommand Events

--- acervo-biblioteca-mb/GestaoReservasCommand/Events/EventName.cs
+++ acervo-biblioteca-mb/GestaoReservasCommand/Events/EventName.cs
@@ -6,12 +6,20 @@ namespace GestaoReservasCommand.Events
 
         public string Value { get; set; }
 
+        // Pedido de Reserva
+        public static EventName ReservaRecebida { get { return new EventName("reserva_recebida"); } }
+        public static EventName ReservaNaoRealizada { get { return new EventName("reserva_nao_realizada"); } }
+        public static EventName ReservaRealizada { get { return new EventName("reserva_realizada"); } }
         public static EventName EmprestimoSobreposto { get { return new EventName("emprestimo_sobreposto"); } }
         public static EventName EmprestimoNaoSobreposto { get { return new EventName("emprestimo_nao_sobreposto"); } }
         public static EventName UtenteAutorizado { get { return new EventName("utente_autorizado"); } }
         public static EventName UtenteNaoAutorizado { get { return new EventName("utente_nao_autorizado"); } }
-        public static EventName ReservaRecebida { get { return new EventName("reserva_recebida"); } }
-        public static EventName ReservaNaoRealizada { get { return new EventName("reserva_nao_realizada"); } }
-        public static EventName ReservaRealizada { get { return new EventName("reserva_realizada"); } }
+
+        // Pedido de Emprestimo
+        public static EventName EmprestimoRecebido { get { return new EventName("emprestimo_recebido"); } }
+        public static EventName ExisteReservaUtente { get { return new EventName("existe_reserva_utente"); } }
+        public static EventName ExisteReservas { get { return new EventName("existe_reserva"); } }
+        public static EventName NaoExisteReservas { get { return new EventName("nao_existe_reserva"); } }
+
     }
 }
