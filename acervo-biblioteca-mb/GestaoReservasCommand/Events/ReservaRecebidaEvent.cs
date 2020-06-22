5bf706a8281ce24282a7d7bf19e1badf032db897
Rute Santos <1160663@isep.ipp.pt>
2020-06-22 11:15:22 +0100

    GestaoReservasCommand Event

--- acervo-biblioteca-mb/GestaoReservasCommand/Events/ReservaRecebidaEvent.cs
+++ acervo-biblioteca-mb/GestaoReservasCommand/Events/ReservaRecebidaEvent.cs
@@ -10,7 +10,8 @@ namespace GestaoReservasCommand.Event
         public string obra { get; set; }
         
         public ReservaRecebidaEvent(){}
-        public ReservaRecebidaEvent(string utente, DateTime dataInicio, DateTime dataFim, string obra){
+        public ReservaRecebidaEvent(string utente, DateTime dataInicio, DateTime dataFim, string obra, string streamId){
+            this.streamId = streamId;
             this.utente = utente;
             this.dataInicio = dataInicio;
             this.dataFim = dataFim;
