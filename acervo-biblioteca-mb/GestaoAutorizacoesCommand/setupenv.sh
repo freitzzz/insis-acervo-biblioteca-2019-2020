7e7bc2586097bc7fa47a0fedb18544e7279d86bf
Rute Santos <1160663@isep.ipp.pt>
2020-06-23 11:15:28 +0100

    GestaoAutorizacoesCommand setupenv

--- acervo-biblioteca-mb/GestaoAutorizacoesCommand/setupenv.sh
+++ acervo-biblioteca-mb/GestaoAutorizacoesCommand/setupenv.sh
@@ -5,3 +5,5 @@ export RABBIT_MQ_CONNECTION_URL='amqp://guest:guest@insis-rabbitmq.northeurope.c
 export REPOR_ESTADO_EXCHANGE=repor_estado
 
 export GU_QUERY_HOST=http://localhost:8081
+
+export ESB_HOST=http://localhost:8280
