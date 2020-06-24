f1acc2d8a83001dbd3851701ed5409cc8ac91d8e
Rute Santos <1160663@isep.ipp.pt>
2020-06-24 17:00:47 +0100

    GestaoReservasQuery

--- acervo-biblioteca-mb/GestaoReservasQuery/Startup.cs
+++ acervo-biblioteca-mb/GestaoReservasQuery/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
 using GestaoReservasQuery.Configurations;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace GestaoReservasQuery
 {
@@ -20,14 +21,33 @@ namespace GestaoReservasQuery
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
 
             int chosenDB = Configuration.GetValue("ChosenDB", 1);
-            DataConfiguration.configure((DataProviderEnum) chosenDB, Configuration, services);
+            DataConfiguration.configure((DataProviderEnum)chosenDB, Configuration, services);
 
             ServiceConfiguration.configure(Configuration, services);
             RabbitMqConfiguration.configure(Configuration, services);
+
+            services.AddCors();
+
+            // ********************
+            // Setup CORS
+            // ********************
+            // var corsBuilder = new CorsPolicyBuilder();
+            // corsBuilder.AllowAnyHeader();
+            // corsBuilder.AllowAnyMethod();
+            // corsBuilder.AllowAnyOrigin(); // For anyone access.
+            // //corsBuilder.WithOrigins("http://localhost:56573"); // for a specific url. Don't add a forward slash on the end!
+            // corsBuilder.AllowCredentials();
+
+            // services.AddCors(options =>
+            // {
+            //     options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,16 +58,25 @@ namespace GestaoReservasQuery
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            //app.UseMvc();
+
+
+            // ********************
+            // USE CORS - might not be required.
+            // ********************
+            // app.UseCors("SiteCorsPolicy");
         }
     }
 }
